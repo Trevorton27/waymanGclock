b7e0bea0167d0e9c76793a308d05e0f9e67166d6
DESKTOP-09LODQG\wayma <waymangoree7@gmail.com>
2021-03-27 00:33:13 -0400

    time applied, but doesnt tick

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
+initClock();
+
 function initClock() {
     var now = new Date();
-    var h = now.getHours();
-    var m = now.getMinutes();
-    var s = now.getSeconds();
-    var meridian = "PM";
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    let sess = "AM";
 
-    if (h == 0) {
-        h = 12;
+
+    if (now.getSeconds() < 10) {
+        seconds = "0" + now.getSeconds();
+    } else {
+        seconds = now.getSeconds();
     }
 
-    if (h > 12) {
-        h = h - 12;
-        meridian = "PM";
+    if (hours == 0) {
+        hours = 12;
     }
 
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : h;
+    if (hours > 12) {
+        hours = hours - 12;
+        sess = "PM";
+    }
 
-    var timeStamp = h + ":" + m + ":" + s + " " + meridian;
-    document.getElementById(displayTime)
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    let timeStamp = [`${hours}:${minutes}:${seconds} ${sess}`];
+
+    document.getElementById("displayTime").textContent = timeStamp;
+};
 
 
-}
+setInterval(initClock(), 1000);
+
+
+
+
 
 
 
