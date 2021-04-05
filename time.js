1a24a17764e8e216a88c4880179c06f040e289e0
Trevor Mearns <spiral272@gmail.com>
2021-04-05 13:53:04 +0900

    ready for code review approval

--- time.js
+++ time.js
@@ -1,79 +1,85 @@
 function initClock() {
-    let now = new Date();
-    let hours = addZero(now.getHours());
-    let minutes = addZero(now.getMinutes());
-    let seconds = addZero(now.getSeconds());
+  const now = new Date();
+  const hours = addZero(now.getHours());
+  const minutes = addZero(now.getMinutes());
+  const seconds = addZero(now.getSeconds());
+  const isAm = hours < 12 || hours === 0;
+  let amPm = isAm ? 'AM' : 'PM';
 
-    let amPm = "AM";
-    amPm = (hours >= 12) ? "PM" : "AM";
-    hours = (hours == 0) ? 12 : ((hours > 12) ? (hours - 12) : hours);
+  document.getElementById(
+    'displayTime'
+  ).textContent = `${displayTwelveHourClock(
+    hours
+  )}:${minutes}:${seconds} ${amPm}`;
+}
 
+function addZero(number) {
+  return number < 10 ? '0' + number : number;
+}
 
-    document.getElementById("displayTime").textContent = `${hours}:${minutes}:${seconds} ${amPm}`;
+function addDateSuffix(number) {
+  if (number < 10 || number > 20) {
+    switch (number % 10) {
+      case 1:
+        return number + 'st';
+      case 2:
+        return number + 'nd';
+      case 3:
+        return number + 'rd';
+    }
+  }
+  return number + 'th';
+}
 
-    setTimeout(function () { initClock(), 1000 });
+function displayTwelveHourClock(hour) {
+  hour = hour >= 13 ? hour - 12 : hour;
 
-    // Date...
+  hour = hour === 0 ? hour + 12 : hour;
+  return hour;
+}
 
-    const today = new Date();
+function initDate() {
+  const today = new Date();
+  const day = days[today.getDay()];
+  const monthName = month[today.getMonth()];
+  const fullYear = [today.getFullYear()];
+  const date = addDateSuffix([today.getDate()]);
 
-    // Days...
+  document.getElementById(
+    'displayDate'
+  ).textContent = `${day}, ${monthName} ${date}, ${fullYear}`;
+}
 
-    const day = [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday',
-        'Sunday',
-    ];
-    const dayName = day[today.getDay()];
+const days = [
+  'Sundays',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday'
+];
 
-
-    //  Months...
-
-    const month = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December',
-    ];
-
-    const monthName = month[today.getMonth()];
-    const fullYear = [today.getFullYear()];
-    const weekDay = [today.getDate()]
-
-    document.getElementById("displayDate").textContent = `${dayName}, ${monthName} ${weekDay}, ${fullYear}`;
-
-};
-
-function addZero(num) {
-    return num < 10 ? "0" + num : num;
-};
+const month = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
 
 initClock();
+initDate();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+setInterval(() => {
+  initClock();
+  initDate();
+}, 1000);
